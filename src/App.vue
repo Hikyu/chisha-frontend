ce63a403db34fb90f5a6f48116b9916ea35c1b46
yukai <yukai>
2018-03-22 18:11:59 +0800

    添加 metrail-design-icon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <div>
-    <md-toolbar class="md-primary md-transparent">
-        <h1 class="md-title">Vue Material</h1>
+  <div class="header">
+    <md-toolbar class="md-transparent" md-elevation="0">
+      <h1 class="md-title">美食与烹饪</h1>
     </md-toolbar>
-    
     <md-content>
-        Your content here
+      <div class="md-layout md-alignment-center-space-around">
+        <div id="search" class="md-layout-item md-size-20">
+          <md-field>
+            <label>Search</label>
+            <md-input></md-input>
+            <md-icon>search</md-icon>
+          </md-field>
+        </div>
+      </div>
     </md-content>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-  }
+export default {
+  name: "App"
+};
 </script>
 
 <style>
-body {
-  background: #f0f0ff
-}
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
 .md-content {
-  padding: 16px;
+  width: 100%;
+  height: 400px;
+  align-items: center;
+  background-image: linear-gradient(
+    45deg,
+    rgb(55, 119, 175),
+    rgb(125, 92, 141) 50%,
+    rgb(231, 50, 88)
+  );
+}
+
+.header {
+  background-image: linear-gradient(
+    45deg,
+    rgb(55, 119, 175),
+    rgb(125, 92, 141) 50%,
+    rgb(231, 50, 88)
+  );
+}
+
+#search {
+  margin-top: 5%;
 }
 </style>
